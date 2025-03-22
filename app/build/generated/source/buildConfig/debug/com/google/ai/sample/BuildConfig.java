c140770c1c41fe7741ec5ee37a86a44d6ca319b6
Toma <tomaandrei98@gmail.com>
2025-03-22 12:18:19 +0200

    initial setup

---
+++ app/build/generated/source/buildConfig/debug/com/google/ai/sample/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.ai.sample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.ai.sample";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String apiKey = "AIzaSyCm7gFRGsW-RSbHWR89ZD4fL9tWdxHXCME";
+}
